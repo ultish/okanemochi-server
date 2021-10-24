dc46f11187f2d2dfbc484010524b90a6009a0268
Jimmy <ultish@gmail.com>
2021-10-24 20:20:29 +1100

    CORS

--- src/main/kotlin/com/ultish/OkanemochiApplication.kt
+++ src/main/kotlin/com/ultish/OkanemochiApplication.kt
@@ -2,11 +2,21 @@ package com.ultish
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class OkanemochiApplication
 
 fun main(args: Array<String>) {
-    runApplication<OkanemochiApplication>(*args)
+   runApplication<OkanemochiApplication>(*args)
+}
 
+// Enable CORS
+@Configuration
+class WebConfig : WebMvcConfigurer {
+   override fun addCorsMappings(registry: CorsRegistry) {
+      registry.addMapping("/**")
+   }
 }
